da82e7ba59c48b0dea42ef5d38b31f6e32d03e24
kavindaperera <perera.kavinda52@gmail.com>
2022-01-22 23:59:17 +0530

    Change app icons

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:food_app/pages/StarterPage.dart';
+import 'package:food_app/pages/starter_page.dart';
 
 void main() => runApp(
       MaterialApp(
